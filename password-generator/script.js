3723423db9b81dc3a070c7dbc563e6f07ae77694
Nicorabi <121673697+Nicorabi@users.noreply.github.com>
2024-12-10 01:16:05 +1100

    Initial commit

---
+++ password-generator/script.js
@@ -0,0 +1,17 @@
+let containerParagraphOne = document.getElementById('container-paragraph-one');
+let containerParagraphTwo = document.getElementById('container-paragraph-two');
+
+function randomPassword() {
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+    let password = '';
+    for (let i = 0; i < 8; i++) {
+        let randomIndex = Math.floor(Math.random() * str.length);
+        password += str[randomIndex];
+    }
+    return password;
+}
+
+function render() {
+    containerParagraphOne.textContent = randomPassword();
+    containerParagraphTwo.textContent = randomPassword();
+}
